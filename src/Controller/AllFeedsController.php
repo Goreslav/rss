d269b44e5ab1701b3d85ae1a527edea9b9438192
lubos <lubos.vlcek@ringier.sk>
2020-10-03 05:27:35 +0200

    + adding feeds and all feed s added

--- src/Controller/AllFeedsController.php
+++ src/Controller/AllFeedsController.php
@@ -2,18 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Feeds;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AllFeedsController extends FeedController
 {
     /**
-     * @Route("/feed/all/", name="all_feeds")
+     * @var array
      */
-    public function index()
+    private $feeds= [];
+    /**
+     * @Route("/feed/all/", name="all_feeds")
+     * @return Response
+     */
+    public function AllFeedAction(): Response
     {
+        $this->getgetAllFedds();
         return $this->render('all_feeds/index.html.twig', [
             'links' => $this->feedNavLinks(),
+            'feeds' => $this->feeds
         ]);
+    }
+
+
+    private function getgetAllFedds(): void
+    {
+        $query= $this->getDoctrine()->getRepository(Feeds::class);
+        $feeds= $query->findAll();
+
+        if (empty($feeds)) {
+            $this->addFlash('warning', 'ziadne linky');
+        }
+        $this->feeds= $feeds;
     }
 }
